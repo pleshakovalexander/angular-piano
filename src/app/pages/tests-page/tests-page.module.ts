6058b0658ff3634e39b89445279c321cef75259a
Alexander Pleshakov <sh.plh@yandex.ru>
2020-10-31 01:19:27 +0300

    add tests page

---
+++ src/app/pages/tests-page/tests-page.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TestsPageComponent } from './tests-page.component';
+
+@NgModule({
+  declarations: [],
+  imports: [CommonModule],
+  exports: [TestsPageComponent],
+  providers: [],
+})
+export class TestsPageModule { }
