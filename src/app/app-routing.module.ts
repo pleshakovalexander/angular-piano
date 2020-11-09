14bccc21fd0fcf1d23a5829d99a050deb6f2469e
Alexander Pleshakov <sh.plh@yandex.ru>
2020-11-10 01:29:16 +0300

    add route for tests page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { TestsPageComponent } from './pages/tests-page/tests-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/tests', pathMatch: 'full' },
+  { path: 'tests', component: TestsPageComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
