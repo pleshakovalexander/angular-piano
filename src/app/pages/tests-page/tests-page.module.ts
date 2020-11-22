9a2f04c4301a94b0ff77d2c547f7d7a09f8b486c
Alexander Pleshakov <sh.plh@yandex.ru>
2020-11-22 16:59:41 +0300

    add start page component

--- src/app/pages/tests-page/tests-page.module.ts
+++ src/app/pages/tests-page/tests-page.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { TestsPageComponent } from './tests-page.component';
 import { ToggleButtonComponent } from './components/toggle-button/toggle-button.component';
 import { FormsModule } from '@angular/forms';
+import { StartTestComponent } from './components/start-test/start-test.component';
 
 @NgModule({
   imports: [CommonModule, FormsModule],
-  declarations: [TestsPageComponent, ToggleButtonComponent]
+  declarations: [TestsPageComponent, ToggleButtonComponent, StartTestComponent]
 })
 export class TestsPageModule { }
