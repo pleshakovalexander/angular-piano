7e13bb2a4856b7e99e115f013e25511f8a961064
Alexander Pleshakov <sh.plh@yandex.ru>
2020-12-21 00:35:33 +0300

    add piano-keys component

---
+++ src/app/shared-components/piano-keys/piano-keys.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-piano-keys',
+  templateUrl: './piano-keys.component.html',
+  styleUrls: ['./piano-keys.component.css']
+})
+export class PianoKeysComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/shared-components/piano-keys/piano-keys/piano-keys.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PianoKeysComponent } from '../piano-keys.component';
+
+
+
+@NgModule({
+  declarations: [PianoKeysComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class PianoKeysModule { }
