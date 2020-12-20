66de3c13595d31234d18ed5bb2a32f90f970f684
Alexander Pleshakov <sh.plh@yandex.ru>
2020-12-20 22:19:43 +0300

    add files to hearing test page

---
+++ src/app/pages/hearing-test-page/hearing-test-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-hearing-test-page',
+  templateUrl: './hearing-test-page.component.html',
+  styleUrls: ['./hearing-test-page.component.css']
+})
+export class HearingTestPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/pages/hearing-test-page/hearing-test-page.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HearingTestPageComponent } from './hearing-test-page.component';
+
+
+
+@NgModule({
+  declarations: [HearingTestPageComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class HearingTestPageModule { }
