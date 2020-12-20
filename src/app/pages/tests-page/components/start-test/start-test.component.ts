1a501c4b8586e61e9500be26a775181f05807d16
Alexander Pleshakov <sh.plh@yandex.ru>
2020-12-20 15:05:07 +0300

    implement basic functionality in start test component

--- src/app/pages/tests-page/components/start-test/start-test.component.ts
+++ src/app/pages/tests-page/components/start-test/start-test.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Signature } from 'src/app/models';
+import { getAvailableOctaves, Octave } from 'src/app/utils';
 
 @Component({
   selector: 'app-start-test',
@@ -6,10 +8,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./start-test.component.css']
 })
 export class StartTestComponent implements OnInit {
-
-  constructor() { }
+  questionNumbersList: number[] = [];
+  octaves: Octave[] = [];
 
   ngOnInit(): void {
+    this.octaves = getAvailableOctaves(Signature.sharp);
+    let num = 10;
+    while (num <= 50) {
+      this.questionNumbersList.push(num);
+      num += 5;
+    }
   }
 
+  buttonToggled(value: Signature): void {
+    this.octaves = getAvailableOctaves(value);
+  }
 }
