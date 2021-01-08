62ad6a95cbad8872ea8b00a619620ad57b7cb609
Alexander Pleshakov <sh.plh@yandex.ru>
2021-01-08 03:03:01 +0300

    navigate from tests page to hearing test

--- src/app/pages/tests-page/components/start-test/start-test.component.ts
+++ src/app/pages/tests-page/components/start-test/start-test.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { Router } from '@angular/router';
 import { Signature } from 'src/app/models';
 import { getAvailableOctaves, Octave } from 'src/app/utils';
 
@@ -11,16 +13,34 @@ export class StartTestComponent implements OnInit {
   questionNumbersList: number[] = [];
   octaves: Octave[] = [];
 
+  selectedOctave: FormControl;
+  selectedNumberOfQuestions: FormControl;
+
+  constructor(private router: Router) {}
+
   ngOnInit(): void {
     this.octaves = getAvailableOctaves(Signature.sharp);
+    this.selectedOctave = new FormControl(this.octaves[0].code);
     let num = 10;
     while (num <= 50) {
       this.questionNumbersList.push(num);
       num += 5;
     }
+    this.selectedNumberOfQuestions = new FormControl(
+      this.questionNumbersList[0]
+    );
   }
 
   buttonToggled(value: Signature): void {
     this.octaves = getAvailableOctaves(value);
+    this.selectedOctave.setValue(this.octaves[0].code);
+  }
+
+  onSubmit() {
+    const questions: number = this.selectedNumberOfQuestions.value;
+    const octave: string = this.selectedOctave.value;
+    this.router.navigate(['/hearing-test'], {
+      queryParams: { octave, questions }
+    });
   }
 }
--- src/app/pages/tests-page/tests-page.module.ts
+++ src/app/pages/tests-page/tests-page.module.ts
@@ -2,11 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TestsPageComponent } from './tests-page.component';
 import { ToggleButtonComponent } from './components/toggle-button/toggle-button.component';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { StartTestComponent } from './components/start-test/start-test.component';
 
 @NgModule({
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule],
   declarations: [TestsPageComponent, ToggleButtonComponent, StartTestComponent]
 })
-export class TestsPageModule { }
+export class TestsPageModule {}
