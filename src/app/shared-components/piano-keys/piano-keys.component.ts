d931b6d6c66eba321d0bff5b18a170fd5c2d0b8e
Alexander Pleshakov <sh.plh@yandex.ru>
2021-01-08 03:28:38 +0300

    add piano functional to piano keys component

--- src/app/shared-components/piano-keys/piano-keys.component.ts
+++ src/app/shared-components/piano-keys/piano-keys.component.ts
@@ -1,15 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { ThrowStmt } from '@angular/compiler';
+import { not } from '@angular/compiler/src/output/output_ast';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { NoteMark, OctaveModelHelper, OctaveViewModel } from 'src/app/utils/piano';
 
 @Component({
   selector: 'app-piano-keys',
   templateUrl: './piano-keys.component.html',
   styleUrls: ['./piano-keys.component.css']
 })
-export class PianoKeysComponent implements OnInit {
+export class PianoKeysComponent {
+  private _octaveHelper = new OctaveModelHelper();
+  octaves: OctaveViewModel[] = [];
 
-  constructor() { }
+  @Input() set octaveName(name: string){
+    if (name) {
+      this.octaves = this._octaveHelper.getModel(name);
+    } else {
+      this.octaves = [];
+    }
+  };
+  @Input() markedNotes: Map<number, NoteMark>;
 
-  ngOnInit(): void {
+  @Output() keyPressed = new EventEmitter();
+
+  handleKeyPressed(note){
+    this.keyPressed.emit(note);
+  }
+
+  getNoteClass(noteMidi: number, noteClassObj: any): any{
+    if(this.markedNotes.has(noteMidi)){
+      return  Object.assign(noteClassObj, {[this.markedNotes.get(noteMidi)]:true} )
+    }
+    return noteClassObj;
   }
 
 }
