b13b115a198b83072fc40af8f00c2941907ccee8
Alexander Pleshakov <sh.plh@yandex.ru>
2021-01-08 03:01:54 +0300

    add hearing test route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HearingTestPageComponent } from './pages/hearing-test-page/hearing-test-page.component';
 import { TestsPageComponent } from './pages/tests-page/tests-page.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/tests', pathMatch: 'full' },
-  { path: 'tests', component: TestsPageComponent }
+  { path: 'tests', component: TestsPageComponent },
+  { path: 'hearing-test', component: HearingTestPageComponent }
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
