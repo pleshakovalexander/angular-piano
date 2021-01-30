e4e9e4e928cd42c2fe040626ae910d5dff7cd7df
Alexander Pleshakov <sh.plh@yandex.ru>
2021-01-30 17:33:51 +0300

    add numberOfCorrectAnswers to CurrentTestService

--- src/app/services/current-test.service.ts
+++ src/app/services/current-test.service.ts
@@ -8,40 +8,54 @@ export enum CurrentTestEvent {
   Finish
 }
 
-interface CurrentTestInfo {
+export interface CurrentTestInfo {
   octaveName: string;
   currentQuestion: number;
   numberOfQuestions: number;
+  numberOfCorrectAnswers: number;
 }
 
 @Injectable({
   providedIn: 'root'
 })
 export class CurrentTestService {
-  private readonly _info = new BehaviorSubject<CurrentTestInfo>(null);
-  public info: Observable<CurrentTestInfo> = this._info.pipe(skip(1));
+  private readonly _info$ = new BehaviorSubject<CurrentTestInfo>(null);
+  public info$: Observable<CurrentTestInfo> = this._info$.pipe(skip(1));
 
-  private readonly _events = new Subject<CurrentTestEvent>();
-  public events: Observable<CurrentTestEvent> = this._events.asObservable();
+  private readonly _events$ = new Subject<CurrentTestEvent>();
+  public events$: Observable<CurrentTestEvent> = this._events$.asObservable();
+
+  private _isTestEnded: boolean;
+  public get isTestEnded(): boolean {
+    return this._isTestEnded;
+  }
 
   init(octaveName: string, numberOfQuestions: number): void {
+    this._isTestEnded = true;
     const i: CurrentTestInfo = {
       currentQuestion: 0,
       octaveName: octaveName,
-      numberOfQuestions: numberOfQuestions
+      numberOfQuestions: numberOfQuestions,
+      numberOfCorrectAnswers: 0
     };
-    this._info.next(i);
-    this._events.next(CurrentTestEvent.Init);
+    this._info$.next(i);
+    this._events$.next(CurrentTestEvent.Init);
   }
 
-  nextQuestion(): void {
-    const i = this._info.getValue();
+  nextQuestion(correct: boolean): void {
+    const i = this._info$.getValue();
     i.currentQuestion++;
-    this._info.next(i);
-    this._events.next(CurrentTestEvent.NextQuestion);
+    if (correct) {
+      i.numberOfCorrectAnswers++;
+    }
+    this._info$.next(i);
+    this._events$.next(CurrentTestEvent.NextQuestion);
   }
 
   finish(): void {
-    this._events.next(CurrentTestEvent.Finish);
+    this._isTestEnded = true;
+    const i = this._info$.getValue();
+    i.currentQuestion = i.numberOfQuestions;
+    this._events$.next(CurrentTestEvent.Finish);
   }
 }
