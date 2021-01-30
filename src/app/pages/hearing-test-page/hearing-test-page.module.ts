e1a2b14b8974ba9dd6ce3b9a024b9077f536acbf
Alexander Pleshakov <sh.plh@yandex.ru>
2021-01-30 18:19:53 +0300

    add imports to hearing test

--- src/app/pages/hearing-test-page/hearing-test-page.module.ts
+++ src/app/pages/hearing-test-page/hearing-test-page.module.ts
@@ -1,13 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HearingTestPageComponent } from './hearing-test-page.component';
-
-
+import { TestBaseModule } from 'src/app/shared-components/test-base/test-base.module';
+import { PianoKeysModule } from 'src/app/shared-components/piano-keys/piano-keys.module';
 
 @NgModule({
   declarations: [HearingTestPageComponent],
-  imports: [
-    CommonModule
-  ]
+  imports: [CommonModule, TestBaseModule, PianoKeysModule]
 })
-export class HearingTestPageModule { }
+export class HearingTestPageModule {}
