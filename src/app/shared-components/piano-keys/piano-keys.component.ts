4b611954828323ba7cc3e30d2b353218537e2c81
Alexander Pleshakov <sh.plh@yandex.ru>
2021-01-30 18:16:50 +0300

    format piano keys

--- src/app/shared-components/piano-keys/piano-keys.component.ts
+++ src/app/shared-components/piano-keys/piano-keys.component.ts
@@ -1,7 +1,9 @@
-import { ThrowStmt } from '@angular/compiler';
-import { not } from '@angular/compiler/src/output/output_ast';
 import { Component, EventEmitter, Input, Output } from '@angular/core';
-import { NoteMark, OctaveModelHelper, OctaveViewModel } from 'src/app/utils/piano';
+import {
+  NoteMark,
+  OctaveModelHelper,
+  OctaveViewModel
+} from 'src/app/utils/piano';
 
 @Component({
   selector: 'app-piano-keys',
@@ -12,26 +14,27 @@ export class PianoKeysComponent {
   private _octaveHelper = new OctaveModelHelper();
   octaves: OctaveViewModel[] = [];
 
-  @Input() set octaveName(name: string){
+  @Input() set octaveName(name: string) {
     if (name) {
       this.octaves = this._octaveHelper.getModel(name);
     } else {
       this.octaves = [];
     }
-  };
+  }
   @Input() markedNotes: Map<number, NoteMark>;
 
   @Output() keyPressed = new EventEmitter();
 
-  handleKeyPressed(note){
+  handleKeyPressed(note) {
     this.keyPressed.emit(note);
   }
 
-  getNoteClass(noteMidi: number, noteClassObj: any): any{
-    if(this.markedNotes.has(noteMidi)){
-      return  Object.assign(noteClassObj, {[this.markedNotes.get(noteMidi)]:true} )
+  getNoteClass(noteMidi: number, noteClassObj: any): any {
+    if (this.markedNotes.has(noteMidi)) {
+      return Object.assign(noteClassObj, {
+        [this.markedNotes.get(noteMidi)]: true
+      });
     }
     return noteClassObj;
   }
-
 }
