005c1ec7d881e4816e229d1e18e821ddff0693ee
Alexander Pleshakov <sh.plh@yandex.ru>
2021-01-30 17:34:10 +0300

    implement base in TestBaseComponent

--- src/app/shared-components/test-base/test-base.component.ts
+++ src/app/shared-components/test-base/test-base.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import {
+  CurrentTestInfo,
+  CurrentTestService
+} from 'src/app/services/current-test.service';
 
 @Component({
   selector: 'app-test-base',
@@ -6,10 +11,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./test-base.component.css']
 })
 export class TestBaseComponent implements OnInit {
-
-  constructor() { }
+  info$: Observable<CurrentTestInfo>;
+  constructor(private currentTestService: CurrentTestService) {}
 
   ngOnInit(): void {
+    this.info$ = this.currentTestService.info$;
   }
 
+  next(): void {
+    this.currentTestService.nextQuestion(false);
+  }
+
+  showEndModal(): void {}
+
+  get testEnded(): boolean {
+    return this.currentTestService.isTestEnded;
+  }
 }
