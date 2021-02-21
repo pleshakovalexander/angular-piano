025756b94d59eb52d4e2a139b87579bd59d297cc
Alexander Pleshakov <sh.plh@yandex.ru>
2021-02-21 20:02:37 +0300

    correct import sampler service in hearing test

--- src/app/pages/hearing-test-page/hearing-test-page.module.ts
+++ src/app/pages/hearing-test-page/hearing-test-page.module.ts
@@ -1,11 +1,23 @@
-import { NgModule } from '@angular/core';
+import { APP_INITIALIZER, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HearingTestPageComponent } from './hearing-test-page.component';
 import { TestBaseModule } from 'src/app/shared-components/test-base/test-base.module';
 import { PianoKeysModule } from 'src/app/shared-components/piano-keys/piano-keys.module';
+import { SamplerService } from 'src/app/services/sampler.service';
 
 @NgModule({
   declarations: [HearingTestPageComponent],
-  imports: [CommonModule, TestBaseModule, PianoKeysModule]
+  imports: [CommonModule, TestBaseModule, PianoKeysModule],
+  providers: [
+    SamplerService,
+    {
+      provide: APP_INITIALIZER,
+      useFactory: (sampleService: SamplerService) => {
+        return () => sampleService.init();
+      },
+      deps: [SamplerService],
+      multi: true
+    }
+  ]
 })
 export class HearingTestPageModule {}
