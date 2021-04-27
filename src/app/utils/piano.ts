6f02775f9bf199e73bfcaf5df65ff6baaa6ff352
Alexander Pleshakov <sh.plh@yandex.ru>
2021-04-27 21:56:24 +0300

    add setCSSClassToDefault method to note class

--- src/app/utils/piano.ts
+++ src/app/utils/piano.ts
@@ -1,51 +1,55 @@
-import { scale } from "@tonaljs/scale";
+import { scale } from '@tonaljs/scale';
 import { toMidi } from '@tonaljs/midi';
 import { octaveNames } from './notes';
 
 export class OctaveModelHelper {
   private allNotes: string[];
   constructor() {
-    // c chromatic scale contains all notes 
+    // c chromatic scale contains all notes
     this.allNotes = scale('c chromatic').notes;
   }
 
   getModel(octaveName: string): OctaveViewModel[] {
     const notesInOctave = scale(octaveName).notes;
     const model: OctaveViewModel[] = [];
-    octaveNames.forEach(octave => {
-      const currentMidiNotes = notesInOctave.map(n => toMidi(n + octave.index));
+    octaveNames.forEach((octave) => {
+      const currentMidiNotes = notesInOctave.map((n) =>
+        toMidi(n + octave.index)
+      );
       const m: OctaveViewModel = {
         name: octave.name,
-        notes: this.allNotes.map(name => {
+        notes: this.allNotes.map((name) => {
           const noteName = name + octave.index;
           const noteMidi = toMidi(noteName) || 0;
-          const noteDegree = this.noteDegreeToRoman(currentMidiNotes.indexOf(noteMidi) + 1);
-          return new Note(noteName, noteDegree, noteMidi)
+          const noteDegree = this.noteDegreeToRoman(
+            currentMidiNotes.indexOf(noteMidi) + 1
+          );
+          return new Note(noteName, noteDegree, noteMidi);
         })
-      }
+      };
       model.push(m);
-    })
+    });
     return model;
   }
 
   private noteDegreeToRoman(noteNumberDegree: number): string {
     switch (noteNumberDegree) {
       case 1:
-        return "I";
+        return 'I';
       case 2:
-        return "II";
+        return 'II';
       case 3:
-        return "III";
+        return 'III';
       case 4:
-        return "IV";
+        return 'IV';
       case 5:
-        return "V";
+        return 'V';
       case 6:
-        return "VI";
+        return 'VI';
       case 7:
-        return "VII";
+        return 'VII';
       default:
-        return "";
+        return '';
     }
   }
 }
@@ -60,25 +64,29 @@ export class Note {
   public name: string;
   public degree: string;
   public midi: number;
-  constructor(name: string, degree: string = '', midi: number = 0, ) {
+  constructor(name: string, degree: string = '', midi: number = 0) {
     this.name = name;
     this.degree = degree;
     this.midi = midi;
-    this.cssClassObj = this.getCSSClassToDefault();
+    this.cssClassObj = this.getDefaultCSSClass();
   }
 
-  public getCSSClassToDefault(): any {
+  private getDefaultCSSClass(): any {
     const cssClassObj: any = {};
-    if (this.name.includes("b")) {
+    if (this.name.includes('b')) {
       cssClassObj.black = true;
     } else {
       cssClassObj.white = true;
     }
     return cssClassObj;
   }
+
+  setCSSClassToDefault(): void {
+    this.cssClassObj = this.getDefaultCSSClass();
+  }
 }
 
 export enum NoteMark {
   wrong = 'wrong',
-  correct = 'correct',
+  correct = 'correct'
 }
