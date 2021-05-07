895311ca47319c240549654bd8212ec4bd517787
Alexander Pleshakov <sh.plh@yandex.ru>
2021-05-07 15:07:04 +0300

    fix  result-progress component show progress

--- src/app/shared-components/test-base/components/result-progress/result-progress.component.ts
+++ src/app/shared-components/test-base/components/result-progress/result-progress.component.ts
@@ -1,27 +1,37 @@
-import { Component, Input, OnInit } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  Component,
+  Input,
+  OnInit
+} from '@angular/core';
 
 @Component({
   selector: 'app-result-progress',
   templateUrl: './result-progress.component.html',
-  styleUrls: ['./result-progress.component.css']
+  styleUrls: ['./result-progress.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ResultProgressComponent implements OnInit {
   @Input() percentage: number;
   offset: number;
   strokeDasharray: string;
 
+  radius = 105;
+
   private circumference: number;
 
-  constructor() {}
+  constructor(private changeDetectionRef: ChangeDetectorRef) {}
 
   ngOnInit(): void {
-    const c = 2 * Math.PI * 105;
+    const c = 2 * Math.PI * this.radius;
     this.circumference = c;
     this.strokeDasharray = `${c} ${c}`;
     this.offset = c;
     setTimeout(() => {
       this.offset =
         this.circumference - (this.percentage / 100) * this.circumference;
+      this.changeDetectionRef.detectChanges();
     }, 100);
   }
 }
