c3716638ffcdf3c5839574caead52957c635a10e
pleshakovalexander <50041552+pleshakovalexander@users.noreply.github.com>
2021-09-10 01:05:06 +0300

    use font icons in test base component

--- src/app/shared-components/test-base/test-base.module.ts
+++ src/app/shared-components/test-base/test-base.module.ts
@@ -3,10 +3,9 @@ import { CommonModule } from '@angular/common';
 import { TestBaseComponent } from './test-base.component';
 import { ResultProgressComponent } from './components/result-progress/result-progress.component';
 import { TestResultsComponent } from './components/test-results/test-results.component';
-import { SvgIconsModule } from '../svg-icons/svg-icons.module';
 
 @NgModule({
-  imports: [CommonModule, SvgIconsModule],
+  imports: [CommonModule],
   declarations: [
     TestBaseComponent,
     ResultProgressComponent,
