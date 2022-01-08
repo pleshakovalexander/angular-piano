0072a8a5919e19a151a9279ad00d3579b6d2e437
pleshakovalexander <50041552+pleshakovalexander@users.noreply.github.com>
2022-01-08 17:28:09 +0300

    remove old svg icon components

--- src/app/shared-components/svg-icons/svg-icon-play/svg-icon-play.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: '[svg-icon-play]',
-  templateUrl: './play.svg'
-})
-export class SvgIconPlayComponent {
-  constructor() {}
-}
